fdc64ab846a20410c2b30cad71a64bf0614ea872
SYTSMA4 <sytsma4@tritons.iowacentral.edu>
2022-10-10 10:43:38 -0500

    Gooder

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -19,48 +19,48 @@ void Header();
 
 //Precondition:     Include libraries fstrem, string, iomanip, and iostream. Pos and Neg must be called
 //Postcondition:    Gets called to main
-float FileInput();
+void BarGraph();
 
 //Precondition:     File Input must be called
 //Postcondition:    s
-float StarCalc();
+void StarCalc();
 
 //Precondition:     File Input must be called
 //Postcondition:    s
-float StarCalcNeg();
+void StarCalcNeg();
 
 //Precondition:     Include library iostream, iomanip. FileInput, Pos, and StarCalc must be called. Have values stored in temp.
 //Postcondition:    Sets width at 0 and outputs |.
-float Output();
+void Output();
 
 //Precondition:     Include library iostream, iomanip. FileInput, Neg, and StarCalcNeg must be called. Have values stored in temp.
 //Postcondition:    Sets width at 0 and outputs |.
-float OutputNeg();
-//Precondition:     Output must be called
-//Postcondition:    s
-float StarOutput();
-//Precondition:     OutputNeg must be called
-//Postcondition:    s
-float StarOutputNeg();
+void OutputNeg();
+
+//Precondition:     Include library iostream, iomanip. FileInput, Pos, StarCalc, and Output must be called. Have values stored in temp.
+//Postcondition:    Outputs * to the right of |.
+void StarOutput();
+
+//Precondition:     Include library iostream, iomanip. FileInput, Neg, StarCalcNeg, and OutputNeg must be called. Have values stored in temp.
+//Postcondition:    Outputs * to the left of |.
+void StarOutputNeg();
+
 //Precondition:     File Input must be called
 //Postcondition:    s
-float Pos();
+void Pos();
 //Precondition:     File Input must be called
 //Postcondition:    s
-float Neg();
-int InTemp;
+void Neg();
 int temp;
 int star;
-int starR;
-int i;
 
 int main() {
     Header();
-    FileInput();
+    BarGraph();
     
     return 0;
 }
-float FileInput(){
+void BarGraph(){
     ifstream infile;
     infile.open(inputFile);
     
@@ -77,60 +77,52 @@ float FileInput(){
     cout << endl;
 
     infile.close();
-    return 0;
 }
-float StarCalc(){
+void StarCalc(){
+    int starR;
     star = temp / 3;
     starR = temp % 3;
     if (starR == 2)
         star = star + 1;
-    
-    return 0;
 }
-float StarCalcNeg(){
+void StarCalcNeg(){
+    int starR;
     star = -temp / 3;
     starR = -temp % 3;
     if (starR == 2)
         star = star + 1;
-    
-    return 0;
 }
 void Header(){
     cout << "                        Temperature" << endl;
     cout << setw(10) << "-30" << setw(10) << "0" << setw(10) << "30" << setw(10) << "60" << setw(10) << "90" << setw(10) << "120";
 }
-float Output(){
+void Output(){
     if (temp < 100 && temp > 9)
         cout << setw(18) << "|";
     if (temp < 10 && temp > 0)
         cout << setw(19) << "|";
     if (temp < 121 && temp > 99)
         cout << setw(17) << "|";
-    
-    return 0;
 }
-float OutputNeg(){
+void OutputNeg(){
     if (temp <= -10 && temp >= -30){
         cout << setw(17-star);
     }
     if (temp < 0 && temp >= -9){
         cout << setw(18-star);
     }
-    return 0;
 }
-float StarOutput(){
+void StarOutput(){
+    int i;
     for (int i = 1; i <= star; i++)
         cout << "*";
-    
-    return 0;
 }
-float StarOutputNeg(){
+void StarOutputNeg(){
+    int i;
     for (int i = 1; i <= star; i++)
         cout << "*";
-    
-    return 0;
 }
-float Pos(){
+void Pos(){
     if (temp >= 0 && temp <= 120)
     {
         cout << endl << temp;
@@ -138,9 +130,8 @@ float Pos(){
         Output();
         StarOutput();
     }
-    return 0;
 }
-float Neg(){
+void Neg(){
     if (temp >= -30 && temp <= -1){
         cout << endl << temp;
         StarCalcNeg();
@@ -148,5 +139,4 @@ float Neg(){
         StarOutputNeg();
         cout << "|";
     }
-    return 0;
 }
